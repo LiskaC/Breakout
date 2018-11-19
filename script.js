e7e31415fff48d4aa2e26f24427068821766eaf4
Yelizaveta Crofts <yscrofts@hotmail.com>
2018-11-19 23:32:37 +0100

    buttons imaged and brick structure created

--- script.js
+++ script.js
@@ -7,42 +7,82 @@ window.onload = function () {
   var dy;
   var robot = new Image();
   robot.src = 'images/2drobo.png';
+  var rocket = new Image();
+  rocket.src = 'images/rocket.png';
 
 
-  var launchPadWidth = 50;
-  var launchPadHeight = 10;
+  var blockWidth = 50;
+  var blockHeight = 10;
   var launchPadX;
+
+  var brickRowCount = 3;
+  var brickColumnCount = 5;
+  var brickPadding = 10;
+  var brickOffSetTop = 30;
+  var brickOffSetLeft = 30;
+  var bricks = [];
+  for (var c = 0; c < brickColumnCount; c++);
+  bricks[c] = [];
+  for (var r = 0; r < brickRowCount; r++);
+  bricks[c][r] =
+    {
+      x: (c * (blockWidth + brickPadding)) + brickOffSetLeft,
+      y: (r * (blockHeight + brickPadding)) + brickOffSetTop
+    };
 
   var gameRunning = false;
   document.getElementById("startBtn").onclick = function () {
-    if (gameRunning == true) return;
+    if (gameRunning == true) {
+      gameRunning = false;
+      resetGame();
+    };
     gameRunning = true;
   }
 
-  function drawRobot() {
-    ctx.drawImage(robot, x - 15, y, 30, 30);
+  function getSprite() {
+    return robot;
+  }
+  document.getElementById("getRocket").onclick = function () {
+    getSprite = function () { return rocket };
+    document.getElementById("SpriteTitle").innerText = "Rocket";
+  }
+  document.getElementById("getRobot").onclick = function () {
+    getSprite = function () { return robot };
+    document.getElementById("SpriteTitle").innerText = "Robo";
+  }
+
+  function drawSprite() {
+    ctx.drawImage(getSprite(), x - 15, y, 30, 30);
   };
 
   function drawLaunchPad() {
     ctx.beginPath();
     ctx.fillStyle = '#353551';
-    ctx.fillRect(launchPadX, (canvas.height - launchPadHeight - 20), launchPadWidth, launchPadHeight);
+    ctx.fillRect(launchPadX, (canvas.height - blockHeight - 20), blockWidth, blockHeight);
     ctx.closePath();
   };
+
+  function drawBricks() {
+    ctx.beginPath();
+    ctx.fillStyle = '#353551';
+    ctx.fillRect((bricks[c][r]["x"]), (bricks[c][r]["y"]), blockWidth, blockHeight);
+    ctx.closePath();
+  }
 
   function resetGame() {
     x = canvas.width / 2;
     y = canvas.height - 60;
     dx = 2;
     dy = -2;
-    launchPadX = (canvas.width - launchPadWidth) / 2;
+    launchPadX = (canvas.width - blockWidth) / 2;
   }
 
   function draw() {
     console.log(y + dy > canvas.height - 30);
     ctx.clearRect(0, 0, 480, 320);
-    drawRobot();
+    drawSprite();
     drawLaunchPad();
+    drawBricks();
     if (gameRunning == false) { return; };
     x += dx;
     y += dy;
@@ -57,7 +97,7 @@ window.onload = function () {
 
     //hits the launchPad, changes direction
     else if (y + dy > canvas.height - 60) {
-      if (x > launchPadX && x < launchPadX + launchPadWidth) {
+      if (x > launchPadX && x < launchPadX + blockWidth) {
         dy = -dy;
       }
 
